70c48c5407e54176526c4b332c2e22f1aaf01056
Tamim <th468737@gmail.com>
2023-01-23 16:44:55 +0100

    n done

--- traitement.php
+++ traitement.php
@@ -1,12 +1,10 @@
 <?php
+  session_start();
+    if (isset($_POST['submit'])){
 
-session_start();
-
-if (isset($_POST['submit'])){
-
-    $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
+    $name = filter_input(INPUT_POST,  "name", FILTER_SANITIZE_STRING);
     $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-    $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
+    $qtt = filter_input(INPUT_POST,   "qtt", FILTER_VALIDATE_INT);
 
 
     if($name && $price && $qtt){
@@ -18,6 +16,5 @@ if (isset($_POST['submit'])){
         ];
         $_SESSION['products'][] = $product;
     }
-
 }
 header("location: index.php");
