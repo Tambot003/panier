1b6e7f3ea8f22cf2998106c732ebf3507cd56783
Tamim <th468737@gmail.com>
2023-01-25 16:53:42 +0100

    non done

--- index.php
+++ index.php
@@ -5,37 +5,41 @@
 <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
 </head>
 <body>
+  <div class="centrer">
+  <a id="lin" href="index.php">HOME</a>
+  <a id="lin" href="recap.php">RECAPLUTIVE</a>
+  </div>
 
-  
- <div id="header"> 
+  <div id="header"> 
 AJOUTER LE PRODUIT
 </div>
 <div id="tam">
-  <form action="traitement.php" method="post">
+  <form action="traitement.php?action=addproducts"  method="post">
 
       <label for="name">NOM :
-      <input type="text" name="name" id="name">
+        <input type="text" name="name" id="name">
       </label>
       <br>
       <br>
 
       <label for="price">PRIX :
-      <input type="number" name="price" id="price">
+        <input type="number" name="price" id="price">
       </label>
       <br>
       <br>
 
       <label for="qtt">QUANTITE :
-      <input type="number" name="qtt" id="qtt"><br>
+        <input type="number" name="qtt" id="qtt"><br>
       </label>
     <br>
     <br>
 
     <input type="submit" name="submit" value="Accept">
-  -->
   </form>
   </div>
   <div id="traitment">
-  "<input type="submit" name="submit" value= traitment >
+  <inpuet type="submit" name="submit" value="delet" >
   </div>
+
+
 </body>
--- traitement.php
+++ traitement.php
@@ -1,29 +1,72 @@
 <?php
-  session_start();
+session_start();
 
-  switch($_GET["action"]) {
-    case "addProduct" :
-    
-    $addprudact;
-    if (isset($_POST['submit'])){
-        
+switch($_GET["action"]) {
 
-    $name = filter_input(INPUT_POST,  "name", FILTER_SANITIZE_STRING);
-    $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-    $qtt = filter_input(INPUT_POST,   "qtt", FILTER_VALIDATE_INT);
+    case "addproducts" :
 
+        if(isset($_POST["submit"])) {
+            
+            $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+           
+            $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+           
+            $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
+            
+            if(isset($name) && isset($price) && isset($qtt)) {
+                $product = [
+                    "name" => $name,
+                    "price" => $price,
+                    "qtt" => $qtt,
+                    "total" => $price*$qtt,
+                ];
+                $_SESSION["products"][] = $product;
+            }else{
+              echo "Something very wrong has happened";
+            }
 
-    if($name && $price && $qtt){
-        $product = [
-            "name" => $name,
-            "price" => $price,
-            "qtt" => $qtt,
-            "total" => $price * $qtt
-        ];
-        $_SESSION['products'][] = $product;
+            echo "Product has been Aadded";
+          
+            header("Location: recap.php"); 
+          }
+    break;
+
+    // Decrease Quantity product
+    case "lowerQtt":
+        if($_SESSION['products'][$_GET['id']]['qtt'] > 1){
+		$_SESSION['products'][$_GET['id']]['qtt']--;
+        $_SESSION['products'][$_GET['id']]['total'] -= $_SESSION['products'][$_GET['id']]['price'];
+    }else{
+        unset($_SESSION['products'][$_GET['id']]);
     }
+        header("Location: recap.php"); //Redirige vers recap.php
+        die;
+    break;
+
+    // Increase Quantity product
+    case "addQtt":
+		$_SESSION['products'][$_GET['id']]['qtt']++;
+        $_SESSION['products'][$_GET['id']]['total'] += $_SESSION['products'][$_GET['id']]['price'];
+        header("Location: recap.php");
+        die;
+    break;
+
+
+    // Delet panier
+    case "deletePanier":
+        unset($_SESSION['products'][$_GET['id']]); //unset($_SESSION['products'][$_GET['index']]['total']);
+        header("Location: recap.php"); 
+        die;
+    break;
+
+    // Delete all Qtt product
+    case "deleteAll":
+        unset($_SESSION["products"]); // $_get va prendre comme paramètre ici id qu'on va mettre ligne 55 dans le recap
+        $_SESSION['message'] = "<p>Le produit " . $product['name'] . " a bien été supprimé</p>";
+        header("Location: recap.php"); 
+        die;
+    break;
+
+
 }
-header("location: index.php");
 
-
-  }
